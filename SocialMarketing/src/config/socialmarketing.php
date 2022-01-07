faa32c3e9ff83be5aa1c5541f06d81b010fdf5bf
secureweb16 <secureweb16@gmail.com>
2022-01-07 15:21:15 +0530

    Description Limit Constraint  Other Changes

--- SocialMarketing/src/config/socialmarketing.php
+++ SocialMarketing/src/config/socialmarketing.php
@@ -5,7 +5,10 @@ return [
 	'telegram' => [
 		'base_url' => 'https://api.telegram.org',
 		'access_token' => 'bot2141555629:AAFjb1-4jMWbvdwXmToyEwQq1rUkoFzWCMA',
-		'logs' => false
+		'logs' => false,
+		'api' => [
+			'description_limit' => 1024
+		]
 	]
 
 ];

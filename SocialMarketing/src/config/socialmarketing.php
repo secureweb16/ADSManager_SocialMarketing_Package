acc665de05bb0d48e94b775d6d946ee869c3505c
secureweb16 <secureweb16@gmail.com>
2022-01-18 10:25:36 +0530

    Congif Var updated

--- SocialMarketing/src/config/socialmarketing.php
+++ SocialMarketing/src/config/socialmarketing.php
@@ -4,11 +4,12 @@ return [
 
 	'telegram' => [
 		'base_url' => 'https://api.telegram.org',
-		'access_token' => 'bot2141555629:AAFjb1-4jMWbvdwXmToyEwQq1rUkoFzWCMA',
+		'access_token' => 'bbot5002415190:AAEmRsxG7Zgoi_qgDaZTGVnjCGGoUphvLbQ',
 		'logs' => false,
 		'api' => [
 			'description_limit' => 1024
-		]
+		],
+		'tg_bot_username' => 'MoonLaunch_TGBot'
 	]
 
 ];
